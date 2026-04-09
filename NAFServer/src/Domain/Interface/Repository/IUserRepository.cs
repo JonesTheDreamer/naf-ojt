5551b1f90bf0968b196c0e282a6cf887727b381a
jonesTG00 <heradurajones@gmail.com>
2026-04-09 15:45:48 +0800

    feat: add IUserRepository and UserRepository

---
+++ NAFServer/src/Domain/Interface/Repository/IUserRepository.cs
@@ -0,0 +1,16 @@
+using NAFServer.src.Domain.Entities;
+using NAFServer.src.Domain.Enums;
+
+namespace NAFServer.src.Domain.Interface.Repository
+{
+    public interface IUserRepository
+    {
+        Task<bool> HasRoleAsync(string employeeId, Roles role);
+        Task<List<User>> GetAllAsync();
+        Task<List<UserRole>> GetRolesByEmployeeIdAsync(string employeeId);
+        Task AddAsync(User user, UserRole userRole);
+        Task RemoveRoleAsync(string employeeId, Roles role);
+        Task<List<string>> GetLocationsAsync();
+        Task AssignLocationAsync(string employeeId, string location);
+    }
+}
