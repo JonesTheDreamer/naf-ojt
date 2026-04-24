af334153aa952313732b896dc6302cc903baabcd
Rodel Jones G. Heradura <rgheradura@smcdacon.com>
2026-04-24 10:11:26 +0800

    fix: correct wrong DbSet reference and error messages in UserRoleRepository

---
+++ NAFServer/src/Domain/Interface/Repository/IUserRoleRepository.cs
@@ -0,0 +1,13 @@
+﻿using NAFServer.src.Domain.Entities;
+
+namespace NAFServer.src.Domain.Interface.Repository
+{
+    public interface IUserRoleRepository
+    {
+        Task<List<UserRole>> GetUserRolesAsync(int userId);
+        Task<List<UserRole>> GetUserActiveRolesAsync(int userId);
+        Task<List<UserRole>> AddUserRoleAsync(int userId, int roleId);
+        Task<List<UserRole>> RemoveUserRoleAsync(int userId, int roleId);
+        Task<bool> UserHasRoleAsync(int userId, int roleId);
+    }
+}
---
+++ NAFServer/src/Infrastructure/Persistence/Repositories/UserRoleRepository.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NAFServer.src.Domain.Entities;
+using NAFServer.src.Domain.Interface.Repository;
+
+namespace NAFServer.src.Infrastructure.Persistence.Repositories
+{
+    public class UserRoleRepository : IUserRoleRepository
+    {
+        private readonly AppDbContext _context;
+
+        public UserRoleRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<UserRole>> AddUserRoleAsync(int userId, int roleId)
+        {
+            var roles = await GetUserRolesAsync(userId);
+            var existingRole = roles.FirstOrDefault(r => r.RoleId == roleId);
+            if (existingRole != null)
+            {
+                if (existingRole.IsActive)
+                {
+                    throw new KeyNotFoundException("User already have this role.");
+                }
+                existingRole.SetToActive();
+            }
+            else
+            {
+                _context.UserRoles.Add(new UserRole(userId, roleId));
+            }
+            await _context.SaveChangesAsync();
+            return await GetUserRolesAsync(userId);
+        }
+
+        public async Task<List<UserRole>> GetUserRolesAsync(int userId)
+        {
+            var result = await _context.UserRoles
+            .Include(ur => ur.Role)
+            .Include(ur => ur.User)
+            .Where(ul => ul.UserId == userId)
+            .ToListAsync();
+
+            if (!result.Any())
+                throw new KeyNotFoundException("No user roles found");
+
+            return result;
+        }
+
+        public async Task<List<UserRole>> GetUserActiveRolesAsync(int userId)
+        {
+            var roles = await GetUserRolesAsync(userId);
+            var activeRoles = roles.Where(r => r.IsActive).ToList();
+            return activeRoles;
+        }
+
+        public async Task<List<UserRole>> RemoveUserRoleAsync(int userId, int roleId)
+        {
+            var roles = await GetUserRolesAsync(userId);
+            var existingRole = roles.FirstOrDefault(r => r.RoleId == roleId);
+            if (existingRole != null)
+            {
+                if (!existingRole.IsActive)
+                {
+                    throw new KeyNotFoundException("User is doesn't have this role");
+                }
+                existingRole.SetToInactive();
+                await _context.SaveChangesAsync();
+            }
+            await _context.SaveChangesAsync();
+            return await GetUserRolesAsync(userId);
+        }
+
+        public async Task<bool> UserHasRoleAsync(int userId, int roleId)
+        {
+            return await _context.UserRoles
+            .AnyAsync(ul => ul.UserId == userId && ul.RoleId == roleId && ul.IsActive);
+        }
+    }
+}
