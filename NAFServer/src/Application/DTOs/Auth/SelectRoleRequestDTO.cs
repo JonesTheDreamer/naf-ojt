ee0b143c9055265e86523da27ec44dab976978af
jonesTG00 <heradurajones@gmail.com>
2026-05-10 23:43:41 +0800

    fix(auth): add validation attributes to SelectRoleRequestDTO

--- NAFServer/src/Application/DTOs/Auth/SelectRoleRequestDTO.cs
+++ NAFServer/src/Application/DTOs/Auth/SelectRoleRequestDTO.cs
@@ -1,4 +1,6 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace NAFServer.src.Application.DTOs.Auth
 {
-    public record SelectRoleRequestDTO(string Role);
+    public record SelectRoleRequestDTO([Required][MinLength(1)] string Role);
 }
