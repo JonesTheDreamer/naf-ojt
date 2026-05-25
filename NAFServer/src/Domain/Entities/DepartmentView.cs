52eac7a329c1feb1619b5f62e3f2273f31498ce7
Rodel Jones G. Heradura <rgheradura@smcdacon.com>
2026-05-25 11:10:46 +0800

    feat: update Employee entity for vw_Employees, add DepartmentView entity

---
+++ NAFServer/src/Domain/Entities/DepartmentView.cs
@@ -0,0 +1,9 @@
+namespace NAFServer.src.Domain.Entities
+{
+    public class DepartmentView
+    {
+        public string Id { get; set; }
+        public string DepartmentDesc { get; set; }
+        public string DepartmentHead { get; set; }
+    }
+}
