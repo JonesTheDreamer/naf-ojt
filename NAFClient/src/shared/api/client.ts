9ea59045c4c12054d4bc6874b51df6a814970ffc
Rodel Jones G. Heradura <rgheradura@smcdacon.com>
2026-05-29 14:07:41 +0800

    feat: add axios interceptor to normalize errors into ApiError

--- NAFClient/src/shared/api/client.ts
+++ NAFClient/src/shared/api/client.ts
@@ -1,7 +1,13 @@
-import axios from "axios";
+import axios, { type AxiosError } from "axios";
+import { ApiError } from "@/shared/lib/apiError";
 
 export const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
   headers: { "Content-Type": "application/json" },
   withCredentials: true,
 });
+
+api.interceptors.response.use(
+  (response) => response,
+  (error: AxiosError) => Promise.reject(ApiError.fromAxios(error)),
+);
